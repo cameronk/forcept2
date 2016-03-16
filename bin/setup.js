7dc30275a889f1122343d80678cdfe84a1f7f909
Cameron Kelley <cameron@azuru.me>
2016-03-15 20:18:42 -0500

    Write config to file, apply defaults based on old values

--- bin/setup.js
+++ bin/setup.js
@@ -1,18 +1,48 @@
 var prompt = require('prompt');
+var fs = require('fs');
+
+var data = {};
+
+const storagePath = './storage';
+if(!fs.existsSync(storagePath)) {
+   fs.mkdirSync(storagePath);
+}
+
+const storageFile = storagePath + '/config.json';
+if(fs.existsSync(storageFile)) {
+    console.log("Loading existing configuration values.");
+    var grab = fs.readFileSync(storageFile);
+    data = JSON.parse(grab);
+    for(var key in data) {
+        console.log(" " + key + " => " + data[key]);
+    }
+}
+
+var schema = {
+    properties: {
+        appName: {
+            description: "Application name",
+            type: 'string',
+            default: data.appName || 'Forcept'
+        },
+        port: {
+            description: "Broadcast port",
+            type: 'integer',
+            message: 'The port must be a number.',
+            default: data.port ? parseInt(data.port) : 8080
+        }
+    }
+};
 
 //
 // Start the prompt
 //
 prompt.start();
 
-//
-// Get two properties from the user: username and email
-//
-prompt.get(['username', 'email'], function (err, result) {
-//
-// Log the results.
-//
-console.log('Command-line input received:');
-console.log('  username: ' + result.username);
-console.log('  email: ' + result.email);
+prompt.get(schema, function (err, result) {
+    fs.writeFile(storageFile, JSON.stringify(result), function(err) {
+        if(err) console.error(err);
+        console.log("Wrote configuration to file.");
+        return;
+    });
 });
