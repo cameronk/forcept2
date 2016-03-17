5cd1466854591ae3ec8d648f348698dee1bea244
Cameron Kelley <cameron@azuru.me>
2016-03-17 17:51:55 -0500

    Create patient and visit migrations

---
+++ database/models/Visit.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+    var Visit = sequelize.define('Visit', {
+        patients: {
+            type: DataTypes.TEXT,
+            get: function() {
+                return ModelHelper.jsonGetter(
+                    this.getDataValue('visits')
+                );
+            },
+            set: function(val) {
+                this.setDataValue('visits', ModelHelper.jsonSetter(val));
+            }
+        }
+        stage: DataTypes.STRING
+    }, {
+        classMethods: {
+            associate: function(models) {
+                // associations can be defined here
+            }
+        }
+    });
+    return Visit;
+};
