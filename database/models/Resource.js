8033cfa4fea102c8a04ab27437c1dcbe38b46044
Cameron Kelley <cameron@azuru.me>
2016-03-17 19:58:23 -0500

    Create Resource migration + model

---
+++ database/models/Resource.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+    var Resource = sequelize.define('Resource', {
+        type: DataTypes.STRING,
+        data: DataTypes.TEXT,
+        referenced: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        },
+        uploadedBy: DataTypes.INTEGER
+    }, {
+        classMethods: {
+            associate: function(models) {
+                // associations can be defined here
+            }
+        }
+    });
+    return Resource;
+};
