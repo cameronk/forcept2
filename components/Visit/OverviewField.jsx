6e09b5e5910a629f7ba60d9e61e5d6f9cd212d7e
Cameron Kelley <ckelley@azurutechnology.com>
2016-05-19 20:10:07 -0500

    Fix header fields not rendering in overview

--- components/Visit/OverviewField.jsx
+++ components/Visit/OverviewField.jsx
@@ -27,65 +27,92 @@ class OverviewField extends BaseComponent {
 
     render() {
 
-        var { props } = this,
-            { value } = props,
-            valueDefined = (value && value.length > 0),
-            iconClass = (valueDefined ? "green check mark" : "red close"),
-            descriptionDOM, resourcesDOM;
+        var { props } = this;
 
-        if(valueDefined) {
-            switch(props.type) {
-                case "select":
-                    if(props.settings && props.settings.multiple) {
-                        if(value.length > 0) {
-                            descriptionDOM = (
-                                <ul>
-                                    {value.map((item) => {
-                                        return (
-                                            <li>{item}</li>
-                                        );
-                                    })}
-                                </ul>
-                            );
-                        }
-                    } else {
-                        descriptionDOM = value.toString();
-                    }
-                    break;
-                case "header":
-                    return (
-                        <div className="item grey">
-                            <h5 className="right">{props.name || "Untitled header"}</h5>
-                        </div>
-                    );
-                    break;
-                case "file":
-                    resourcesDOM = value.map(({ type, id, ext }) => {
-                        return (
-                            <div className="ui fluid card">
-                                <div className="ui fluid image">
-                                    <img src={["/resources/", id, ext].join("")} />
-                                </div>
-                            </div>
-                        );
-                    });
-                    break;
-                default:
-                    descriptionDOM = value.toString();
-                    break;
-            }
-        }
+        /*
+         * We ignore value if this is a non-value-bearing field (i.e. header)
+         */
+        if(props.type === "header") {
 
-        return (
-            <div className="item">
-                <i className={iconClass + " icon"}></i>
-                <div className="content">
-                    <span className="header">{upperFirst(props.name || "Untitled field")}</span>
-                    <div className="description">{descriptionDOM}</div>
+            return (
+                <div className="item grey">
+                    <h5 className="right">{props.name || "Untitled header"}</h5>
                 </div>
-                {resourcesDOM}
-            </div>
-        );
+            );
+
+        } else {
+
+            var { value } = props,
+                valueDefined = (value && value.length > 0),
+                iconClass = (valueDefined ? "green check mark" : "red close"),
+                descriptionDOM, resourcesDOM;
+
+            /*
+             * Check if a value is defined.
+             */
+            if(valueDefined) {
+
+                switch(props.type) {
+
+                    /*
+                     * Convert Select type (array of selected values)
+                     * into a list.
+                     */
+                    case "select":
+                        if(props.settings && props.settings.multiple) {
+                            if(value.length > 0) {
+                                descriptionDOM = (
+                                    <ul>
+                                        {value.map((item) => {
+                                            return (
+                                                <li>{item}</li>
+                                            );
+                                        })}
+                                    </ul>
+                                );
+                            }
+                        } else {
+                            descriptionDOM = value.toString();
+                        }
+                        break;
+
+                    /*
+                     * Convert resource into embedded thingy.
+                     */
+                    case "file":
+                        resourcesDOM = value.map(({ type, id, ext }) => {
+                            return (
+                                <div className="ui fluid card">
+                                    <div className="ui fluid image">
+                                        <img src={["/resources/", id, ext].join("")} />
+                                    </div>
+                                </div>
+                            );
+                        });
+                        break;
+
+                    /*
+                     * Otherwise, it's a string!
+                     */
+                    default:
+                        descriptionDOM = value.toString();
+                        break;
+                }
+
+            }
+
+            return (
+                <div className="item">
+                    <i className={iconClass + " icon"}></i>
+                    <div className="content">
+                        <span className="header">{upperFirst(props.name || "Untitled field")}</span>
+                        <div className="description">{descriptionDOM}</div>
+                    </div>
+                    {resourcesDOM}
+                </div>
+            );
+
+        }
 
     }
 
